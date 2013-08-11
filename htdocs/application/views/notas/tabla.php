9dddf754ddf425a423f385b834bca05a37b69fcd
nacho692@gmail.com <nacho692@gmail.com@92f81365-efb1-1595-591d-9e26c4bf9bca>
2013-08-11 22:10:19 +0000

    tabla view

---
+++ htdocs/application/views/notas/tabla.php
@@ -0,0 +1,27 @@
+
+<div class="nota_tabla">
+    <h1><a href="#"></a> por autor - fecha</h1> 
+    <p>TEXTO DE PARRAFO</p>
+</div>
+<div id="numeros">
+    <ul>
+        <li class="numero_tabla" style="float:left; margin-right: 10px;">
+            <<
+        </li>
+        <li class="numero_tabla" style="float:left; margin-right: 10px;">
+            <
+        </li>
+        <li class="numero_tabla" style="float:left; margin-right: 10px;">
+            <a href="#">1</a>
+        </li>
+       
+        <li class="numero_tabla" style="float:left; margin-right: 10px;">
+            >
+        </li>
+        <li class="numero_tabla" style="float:left; margin-right: 10px;">
+            >>
+        </li>
+    </ul>
+</div>
+
+
