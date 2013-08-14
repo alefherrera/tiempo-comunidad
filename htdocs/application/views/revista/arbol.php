cf24da35414a88bb90b79cb8ef91c051f4f6285f
nacho692@gmail.com <nacho692@gmail.com@92f81365-efb1-1595-591d-9e26c4bf9bca>
2013-08-14 03:10:51 +0000

    Arbol arreglado problema de ul y lis

--- htdocs/application/views/revista/arbol.php
+++ htdocs/application/views/revista/arbol.php
@@ -1,28 +1,26 @@
-<?php
-$ano_anterior = 0;
-foreach ($arbol as $revista) {
-    if ($revista['año'] != $ano_anterior) {
+<ul>
+    <?php
+    $ano_anterior = 0;
+    foreach ($arbol as $revista) {
+        if ($revista['año'] != $ano_anterior) {
+            if ($ano_anterior != 0):
+                ?>
+            </li>
+            </ul>
+            <?php
+        endif;
         $ano_anterior = $revista['año'];
         ?>
-        <ul>
-            <li>
-                <a href='<?php echo '#' . $revista['año'] ?>'> <?php echo $revista['año'] ?></a>
-                <ul>
-                    <li>
-                        <a href='/index.php/revista/<?php echo $revista['mes'] . '/' . $revista['año'] ?>'><?php echo $revista['mes'] . ' - ' . $revista['titulo'] ?></a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
 
-    <?php } else {
-        ?>
-        <ul>
-            <li>
+        <li class='arbol_año'>
+            <a href='<?php echo '#' . $revista['año'] ?>'> <?php echo $revista['año'] ?></a>
+            <ul>
+            <?php } ?>
+            <li class='arbol_mes'>
                 <a href='/index.php/revista/<?php echo $revista['mes'] . '/' . $revista['año'] ?>'><?php echo $revista['mes'] . ' - ' . $revista['titulo'] ?></a>
             </li>
-        </ul>   
-        <?php
-    }
-}
-?>
+            <?php
+        }
+        ?>
+    </ul>
+
