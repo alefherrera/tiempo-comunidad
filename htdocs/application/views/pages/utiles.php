ffffcda924e4c99dce49983c8f9dfee8aee4d856
nacho692@gmail.com <nacho692@gmail.com@92f81365-efb1-1595-591d-9e26c4bf9bca>
2013-09-10 18:02:35 +0000

    Utiles

--- htdocs/application/views/pages/utiles.php
+++ htdocs/application/views/pages/utiles.php
@@ -6,5 +6,110 @@
 ?>
 
 <div class="contenido">
+POLICÍA 911
+• COMISARIA MUNRO
+4512-5544/53/5130/31
+• COMISARIA CARAPACHAY
+4512-5137/38/5556/88
+• COMISARIA V. ADELINA (S.I.)
+4580-4445/56/57/58/59
+
+VICENTE LOPEZ ALERTA
+4711-2935/36
+
+ATENCIÓN MÉDICA
+107 o 4718-2123
+• HOSPI TAL DE  V. LOPEZ
+H. YRIGOYEN 757 - FLORIDA
+•UR GEN CIAS: 4791-7777-
+
+UNIDADES DE ATENCIÓN PRIMARIA
+• Bur man
+Ituzaingo 5725, Carapachay. 4580-6232.
+• Negrete
+Rivera 6001 y V. Loreto, Munro- 4580-6233.
+• Scalise
+Cajaravi lle 4042, V. Adelina. 4512-8140.
+• Sargento Cabral
+Sargento Cabral 3221, Munro. 4580-6235.
+• A. Marzano
+Carlos Villa te 3675, Munro. 4580-6234.
+• Dr. Ricardo B. Aguirre
+Sgto. Baigorria 2461, Munro. 4580-6231.
+• 25 de Mayo
+Obligado 5280, Carapachay.
+
+BOMBEROS 100 o 4795-2222
+• BOMBEROS V. LOPEZ
+4795-2222/2002
+• BOMBEROS CARAPACHAY
+4766-2222
+
+DEFENSA CIVIL
+103 o 4799-0871
+
+MUNICIPALIDAD DE V. LO PEZ
+Av. Maipú 2609, Olivos
+4711-2800 / 4711-2900
+
+DELEGACIONES MUNICIPALES
+VTE. LOPEZ - Horario de atención:
+Lun. a Vie., de 8:30 a 16:00 hs. y sáb.
+de 9:00 a 13:00 hs.
+• Delegación Carapachay:
+Independencia y Ascasubi
+Tel: 4756-3098 -4756-5673
+delegacion.carapachay@vicentelopez.gov.ar
+• Delegación Florida Oeste:
+Av. San Martín 4106 - Florida
+Tel: 4760-5896 / 9268
+delegacion.floridao@vicentelopez.gov.ar
+• Delegación Munro:
+V. Sarsfield 4839 - Munro
+Tel: 4762-1204 / 4756-6464
+delegacion.munro@vicentelopez.gov.ar
+• Delegación Villa Adelina:
+Av. Ader 4057, Tel: 4763-7727
+• Delegación Villa Martelli:
+Laprida 3902 V. Martelli. Tel: 4709-
+1144/5775
+delegacion.martelli@vicentelopez.gov.ar
+• Delegación La Loma de Roca:
+San Lorenzo y Uzal. Tel: 4794-7491 /
+4790-3939.
+SERVICIOS
+• AYSA (Agua), Wineberg 2830,
+Olivos
+6319-3302 / 3310 / 3323
+Reclamos 6333-2482
+• GAS NATURAL
+Fonogas (de 8 a 19)
+0810-333-46226
+Escapes (las 24 horas) 0800-888-
+1137
+• EDENOR
+Av. Maipú 1971, Florida. 4743-1143 /
+4747-4221
+Falta de suministro: 0800-666-4003
+•TRANSPORTE OLIVOS
+(recolección de residuos)
+Habana 55, Villa Martelli. 4709-0044 /
+5475 / 5866. Reclamos 0800-444-
+8672
+• MANTELETRIC
+(alumbrado público) Nicaragua 5510,
+Munro - 4756-1186 / 2635
+• ECOLOGÍA Y MEDIO AMBIENTE
+Uzal 3690, Olivos. 4711-9652 / 53
+• DEFENSOR DEL PUEBLO DE V.
+LÓPEZ
+Pelliza 1401 2º, Olivos 4799-5119 / 5127 /
+5146
+• DEFENSA DEL
+CONSUMIDOR
+Juan B. Justo 1379, Florida. 4513-
+6392/ 3
+20
+SOLIDARIDAD
 
 </div>
